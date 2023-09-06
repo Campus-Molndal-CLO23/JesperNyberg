0c04e452185ab83ac6d021a79b30aa8c60282d8b
Santosnr6 <77623157+Santosnr6@users.noreply.github.com>
2023-09-06 21:41:54 +0200

    Temperaturomvandlare med class extensions

---
+++ Temperaturomvandling/Temperaturomvandling/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Temperaturomvandling
+{
+    internal class Program
+    {
+        private const int celsius = 25;
+        static void Main(string[] args)
+        {
+            Temperaturomvandling();
+
+            Console.Read();
+        }
+
+        public static void Temperaturomvandling()
+        {
+            PrintToConsole($"Celsius: {celsius} grader");
+            PrintToConsole($"Farenheit: {celsius.ConvertToFarenheit()} grader");
+            PrintToConsole($"Kelvin:  {celsius.ConvertToKelvin()} grader");
+        }
+
+        public static void PrintToConsole(string temperature)
+        {
+            Console.WriteLine(temperature);
+        }
+    }
+
+    public static class Extension
+    {
+        public static double ConvertToFarenheit(this int celsius)
+        {
+            return celsius * 1.8 + 32;
+        }
+
+        public static double ConvertToKelvin(this int celsius)
+        {
+            return celsius + 273.15;
+        }
+    }
+}
